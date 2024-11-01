d1ea5b6a778beb20164cdeef71db173aca373480
daegon song <qzom1425@naver.com>
2024-11-01 17:09:45 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace p_1_Test_Prog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtPressureValue = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRxStatus = new System.Windows.Forms.PictureBox();
+            this.lblTxStatus = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.Button_AReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.lblRxStatus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblTxStatus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("돋움", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnStart.Location = new System.Drawing.Point(496, 189);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(152, 48);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "기본 측정";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtPressureValue
+            // 
+            this.txtPressureValue.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtPressureValue.Location = new System.Drawing.Point(33, 153);
+            this.txtPressureValue.Name = "txtPressureValue";
+            this.txtPressureValue.Size = new System.Drawing.Size(105, 39);
+            this.txtPressureValue.TabIndex = 1;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("돋움", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnStop.Location = new System.Drawing.Point(496, 243);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(152, 48);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림체", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(178, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "p-1 프로그램(작업중)";
+            // 
+            // lblRxStatus
+            // 
+            this.lblRxStatus.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblRxStatus.Location = new System.Drawing.Point(570, 153);
+            this.lblRxStatus.Name = "lblRxStatus";
+            this.lblRxStatus.Size = new System.Drawing.Size(13, 13);
+            this.lblRxStatus.TabIndex = 4;
+            this.lblRxStatus.TabStop = false;
+            // 
+            // lblTxStatus
+            // 
+            this.lblTxStatus.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTxStatus.Location = new System.Drawing.Point(609, 153);
+            this.lblTxStatus.Name = "lblTxStatus";
+            this.lblTxStatus.Size = new System.Drawing.Size(13, 13);
+            this.lblTxStatus.TabIndex = 5;
+            this.lblTxStatus.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림체", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(605, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "TX";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림체", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(565, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "RX";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림체", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(493, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "차압계";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Font = new System.Drawing.Font("굴림체", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(33, 114);
+            this.label5.Margin = new System.Windows.Forms.Padding(3);
+            this.label5.MaximumSize = new System.Drawing.Size(200, 150);
+            this.label5.Name = "label5";
+            this.label5.Padding = new System.Windows.Forms.Padding(3);
+            this.label5.Size = new System.Drawing.Size(105, 33);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "측정값";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("돋움", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnExit.Location = new System.Drawing.Point(496, 365);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(152, 48);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "프로그램 종료";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 438);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(660, 22);
+            this.statusStrip1.TabIndex = 12;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Button_AReset
+            // 
+            this.Button_AReset.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Button_AReset.Location = new System.Drawing.Point(81, 381);
+            this.Button_AReset.Name = "Button_AReset";
+            this.Button_AReset.Size = new System.Drawing.Size(75, 23);
+            this.Button_AReset.TabIndex = 13;
+            this.Button_AReset.Text = "AReset";
+            this.Button_AReset.UseVisualStyleBackColor = true;
+            //this.Button_AReset.Click += new System.EventHandler(this.Button_AReset_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(660, 460);
+            this.Controls.Add(this.Button_AReset);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTxStatus);
+            this.Controls.Add(this.lblRxStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtPressureValue);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.lblRxStatus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblTxStatus)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtPressureValue;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox lblRxStatus;
+        private System.Windows.Forms.PictureBox lblTxStatus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button Button_AReset;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.IO.Ports;  // SerialPort 클래스 사용
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;  // Debug.WriteLine()을 사용하기 위한 네임스페이스
+using System.IO;  // 파일 로그를 사용하기 위한 네임스페이스
+using System.Drawing.Drawing2D;  // Region과 GraphicsPath 사용을 위해 필요
+using System.Drawing;
+using System.Timers;
+
+namespace p_1_Test_Prog
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+        private System.Threading.Timer Timer_Com2Tx;  // 데이터를 주기적으로 읽기 위한 타이머
+        private bool isReading = false;  // 데이터 읽기 중인지 확인하는 플래그
+        private string FCom2_TxCmd;
+        private bool isTimerRunning = false; // 타이머 실행 상태를 추적할 변수
+        public Form1()
+        {
+            InitializeComponent();
+
+            // SerialPort 설정
+            serialPort = new SerialPort();
+            serialPort.PortName = "COM12";   // 사용 중인 COM 포트 설정
+            serialPort.BaudRate = 9600;     // 차압계의 BaudRate 동일 적용
+            serialPort.Parity = Parity.None;
+            serialPort.DataBits = 8;
+            serialPort.StopBits = StopBits.One;
+            serialPort.Handshake = Handshake.None;
+            serialPort.ReadTimeout = 500;
+            serialPort.WriteTimeout = 500;
+
+            // 타이머 설정 (주기적으로 데이터를 읽음, 기본 250ms 간격)
+            Timer_Com2Tx = new System.Threading.Timer(ReadPressureData, null, Timeout.Infinite, 250);
+
+            // 초기 통신 상태 표시
+            lblRxStatus.BackColor = System.Drawing.Color.Gray; // RX 초기 상태
+            lblTxStatus.BackColor = System.Drawing.Color.Gray; // TX 초기 상태
+
+            // PictureBox를 원형으로 만드는 메서드 호출
+            MakeCircular(lblRxStatus);
+            MakeCircular(lblTxStatus);
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            // 폼이 로드될 때 실행될 코드를 여기에 작성
+            // 예: 폼 초기화 코드
+        }
+
+        // PictureBox를 원형으로 만드는 메서드
+        private void MakeCircular(PictureBox pictureBox)
+        {
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddEllipse(0, 0, pictureBox.Width - 1, pictureBox.Height - 1);
+            pictureBox.Region = new Region(gp);
+        }
+
+        // Start 버튼 클릭: 데이터 읽기 시작
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen)
+            {
+                serialPort.Open();  // 포트가 열려 있지 않으면 한 번만 연다
+            }
+
+            isReading = true;
+            Timer_Com2Tx.Change(0, 250);  // 250ms 간격으로 데이터 읽기 시작
+            Debug.WriteLine("Started Reading Data.");
+        }
+
+        // Stop 버튼 클릭: 데이터 읽기 중지
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            isReading = false;
+            Timer_Com2Tx.Change(Timeout.Infinite, Timeout.Infinite);  // 타이머 중지
+            Debug.WriteLine("Timer Stopped.");
+            Console.WriteLine("Stop");
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();  // 포트를 닫는다
+            }
+            Debug.WriteLine("Stopped Reading Data.");
+        }
+
+        // 주기적으로 데이터를 읽는 타이머 콜백 함수
+        private void ReadPressureData(object state)
+        {
+            try
+            {
+                if (serialPort.IsOpen)
+                {
+                    // TX 표시: 데이터를 송신 중임을 표시 (빨간색)
+                    UpdateTxStatus(true);
+
+                    // 차압계에 값을 요청하는 명령어 전송
+                    string command = "#G";
+                    serialPort.WriteLine(command);
+
+                    // 잠시 대기 후 TX 상태 해제
+                    Thread.Sleep(100);
+                    UpdateTxStatus(false);
+
+                    // RX 표시: 데이터를 수신 중임을 표시 (초록색)
+                    UpdateRxStatus(true);
+
+                    // 차압계로부터 응답 읽기
+                    string response = serialPort.ReadLine();
+
+                    // RX 상태 해제
+                    UpdateRxStatus(false);
+
+                    // Debug 창에 값 출력
+                    Debug.WriteLine("mH2O: " + response);
+
+                    // 파일로 로그 남기기
+                    LogToFile("mH2O: " + response);
+
+                    // 응답을 처리 (예: 압력 값 추출 및 표시)
+                    UpdatePressureValue(ParsePressureValue(response));
+
+                    // 콘솔 창에 출력
+                    Console.WriteLine("mH2O: " + response);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        // TX 상태 업데이트 메서드
+        private void UpdateTxStatus(bool isActive)
+        {
+            lblTxStatus.BackColor = isActive ? System.Drawing.Color.Red : System.Drawing.Color.Gray;
+        }
+
+        // RX 상태 업데이트 메서드
+        private void UpdateRxStatus(bool isActive)
+        {
+            lblRxStatus.BackColor = isActive ? System.Drawing.Color.Green : System.Drawing.Color.Gray;
+        }
+
+        // 파일로 로그 남기는 메서드
+        private void LogToFile(string message)
+        {
+            File.AppendAllText("log.txt", DateTime.Now + " - " + message + Environment.NewLine);
+        }
+
+        // UI 스레드에서 안전하게 텍스트박스를 업데이트하는 메서드
+        private void UpdatePressureValue(string value)
+        {
+            if (txtPressureValue.InvokeRequired)
+            {
+                txtPressureValue.BeginInvoke(new Action(() =>
+                {
+                    txtPressureValue.Text = value;
+                }));
+            }
+            else
+            {
+                txtPressureValue.Text = value;
+            }
+        }
+
+        private string ParsePressureValue(string response)
+        {
+            if (string.IsNullOrWhiteSpace(response) || response.Length < 2)
+            {
+                return "Invalid response";
+            }
+
+            string numericPart = new string(response.Where(c => char.IsDigit(c) || c == '.' || c == '-' || c == '+').ToArray());
+
+            if (decimal.TryParse(numericPart, System.Globalization.NumberStyles.Any,
+                                 System.Globalization.CultureInfo.InvariantCulture,
+                                 out decimal pressureValue))
+            {
+                return pressureValue.ToString("F2", System.Globalization.CultureInfo.InvariantCulture);
+            }
+
+            return "Invalid response";
+        }
+
+        // 종료 버튼 클릭: 프로그램 종료
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();  // 프로그램 종료
+        }
+
+    }
+}
